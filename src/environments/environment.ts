a5768623a42f45d11bef456dd96537e7d6dcd29b
Ana Paola Montero Cassab <anapao.montero@gmail.com>
2018-11-09 13:51:57 -0400

    Home page

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,16 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAaoIfMj6nIJ1DF2VPvmcLT9W0bheiRAIM",
+    authDomain: "database-17b16.firebaseapp.com",
+    databaseURL: "https://database-17b16.firebaseio.com",
+    projectId: "database-17b16",
+    storageBucket: "database-17b16.appspot.com",
+    messagingSenderId: "1000599862754"
+  }
 };
-
 /*
  * In development mode, to ignore zone related error stack frames such as
  * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
